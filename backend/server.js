fed3f9184a222a90f9eba2cec2c72072773de9fd
Ali Alvarez <alvarezali@hotmail.com>
2026-03-28 15:31:11 -0500

    server.js update: middleware/dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+//middleware
+app.use(express.json());
+app.use(cors())
+app.use(helmet()); //security middleware that sets up various HTTP headers
+app.use(morgan('dev')); // logs the requests
+
+//requests
 app.get('/', (req, res) => {
     res.send('Hello from backend!');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
 });
