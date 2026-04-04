e51ba6eae66fb3be7f22d611d34e1b885ccb6e1c
Ali Alvarez <alvarezali@hotmail.com>
2026-04-04 06:55:18 -0500

    productController.js update 04/04/2026

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,17 +7,23 @@ export const getProducts = async (req, res) => {
             SELECT * FROM products
             ORDER BY created_at DESC
             `;
-
             console.log(products);
             res.status(200).json({success:true, data:products});
-    } catch(error){
+
+    } catch(error) {
         console.log('Error in getProducts function ', error);
         res.status(500).json({success:false, message: 'Internal server error'});
     }
 }
 
 export const getProduct = async (req, res) => {
-    res.send('Hello from GET getProduct');
+    const id = req.params;
+
+    try {
+
+    } catch(error) {
+
+    }
 }
 
 export const createProduct = async (req, res) => {
@@ -28,17 +34,15 @@ export const createProduct = async (req, res) => {
     }
 
     try {
-
+        const newProduct = await sql.query(
+            'INSERT INTO products (name, price, image) VALUES ($1, $2, $3) RETURNING *', 
+            [name, price, image]);
+        res.status(200).json({success:true, data:newProduct});
+        
     } catch(error) { 
         console.log('Error in createProduct function ', error);
         res.status(500).json({success:false, message:'Internal server error'});
     }
-
-
-
-
-
-   
 }
 
 export const updateProduct = async (req, res) => {
@@ -46,5 +50,17 @@ export const updateProduct = async (req, res) => {
 }
 
 export const deleteProduct = async (req, res) => {
-    res.send('Hello from DELETE deleteProduct');
+    const id = req.params;
+
+    try {
+        await sql.query(
+            'DELETE FROM products WHERE id=$1',
+            [id]);
+
+        res.status(200).json({success:true, data:id});
+    } catch(error) {
+        console.log('Error in deleteProduct function ', error);
+        res.status(500).json({success:false, message:'Internal server error'});
+    }
+    
 }
