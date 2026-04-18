a52cb1f3fd15e4aeeab8299c461c11bacb80f1b2
Ali Alvarez <alvarezali@hotmail.com>
2026-04-18 18:06:02 -0500

    arcjet.js update 04/18/2026

--- backend/lib/arcjet.js
+++ backend/lib/arcjet.js
@@ -3,6 +3,24 @@ import dotenv from 'dotenv'
 
 dotenv.config({path: '../../.env'});
 
-const aj = arcjet({
-
+export const aj = arcjet({
+    key: process.env.ARCJET_KEY,
+    characteristics: ['userId'],
+    rules: [
+        //shield protects the app from common attacks e.g. SQL injections, XSS, CSRF attacks
+        shield({mode: 'LIVE'}),
+        detectBot({
+            mode: 'LIVE',
+            //blocks all bots except search engine
+            allow: ['CATEGORY:SEARCH_ENGINE'],
+            //full list at https://arcjet.com/bot-list
+        }),
+        //rate limiting
+        tokenBucket({
+            mode:'LIVE',
+            refillRate:5,
+            interval:10,
+            capacity:10,
+        }),
+    ],
 });
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import sql from './config/db.js'
+import aj from './lib/arcjet.js'
 import productRoutes from './routes/productRoutes.js'
 
 
@@ -16,7 +17,38 @@ app.use(cors())
 app.use(helmet()); //security middleware that sets up various HTTP headers
 app.use(morgan('dev')); // logs the requests
 
-//Product requests
+app.use(async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req, {
+            requested: 1 //specifies that each request consumes 1 token
+        });
+
+        if(decision.isDenied()) {
+            if(decision.reason.isRateLimit()) {
+                res.status(429).json({error: 'Too many requests'});
+            } else if(decision.reason.isBot()) {
+                res.status(403).json({error: 'Bot access denied'});
+            } else {
+                res.status(403).json({error: 'Forbidden'});
+            }
+            return;
+        }
+
+        //check for spoofed bots
+        if(decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+            res.status(403).json({error: 'Spoofed bot detected'});
+            return;
+        }
+
+        next();
+
+    } catch(error) {
+        console.log('Arcjet error ', error);
+        next(error);
+    }
+});
+
+//Product route
 app.use('/api/products', productRoutes);
 
 //function to create a new database if it doesn't exists
